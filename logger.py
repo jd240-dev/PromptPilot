ded047bbb378ea720d387fadb1972341f735a044
jd240-dev <jaydan24sep@gmail.com>
2025-07-02 19:30:28 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,5 +1,15 @@
 import atexit
-from utils import clear_logs
+import os
 
-def init_logger():
-    atexit.register(clear_logs)
+LOG_FILE = "promptpilot.log"
+
+def log(msg):
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        f.write(msg + "\n")
+    print(msg)
+
+def delete_logs():
+    if os.path.exists(LOG_FILE):
+        os.remove(LOG_FILE)
+
+atexit.register(delete_logs)
